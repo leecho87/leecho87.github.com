5878de2702b2e1eaa419c06d1bbf0b3a71e612a6
leecho <leecho87@naver.com>
2018-10-20 16:47:17 +0900

    first commit

---
+++ src/js/work.js
@@ -0,0 +1,109 @@
+{
+    "workList" : [
+        {
+            "name": "알바천국 MO Gulp(sass,uglify) 도입중",
+            "participation": "70%",
+            "description" : "파일관리 및 작업편의성 증가 기대, 알바천국 환경에 맞게 css/task 구성 (node.js/gulp/gulp-sass/imagemin)",
+            "thumb" : "/src/image/thumb1.jpg",
+            "pageUrl" : "https://github.com/leecho87/alba_gulp"
+        },
+        {
+            "name": "천국의알바 PC/MO 신규 구축",
+            "participation": "100%",
+            "description": "사이트 노후로 인한 전체개편, 주타겟이 젊은층으로 다양한 효과 제공 (html5/css3/jQuery)",
+            "thumb" : "/src/image/thumb2.jpg",
+            "pageUrl" : "http://www.alba.co.kr/global/Main.asp"
+        },
+        {
+            "name": "알바천국 PC/MO 공고등록단 개편",
+            "participation": "100%",
+            "description": "매출증대를 위해 서비스 개편, 크로스브라우징(IE7), BEM 방법론 적용 (html/css/jQuery)",
+            "thumb" : "/src/image/thumb3.jpg",
+            "pageUrl" : "#"
+        },
+        {
+            "name": "알바천국 컨벤션 참여",
+            "participation": "60%",
+            "description": "naming기법, css 선언순서, 이미지 규칙 등 알바천국에 맞는 컨벤션 도입 (가이드제작[내부])",
+            "thumb" : "/src/image/thumb4.jpg",
+            "pageUrl" : "#"
+        },
+        {
+            "name": "알바천국 PC/MO 기업서비스 개편",
+            "participation": "100%",
+            "description": "매출증대를 위해 서비스 개편, 크로스브라우징(IE7), (html/css/jQuery)",
+            "thumb" : "/src/image/thumb5.jpg",
+            "pageUrl" : "http://www.alba.co.kr/biz/Main.asp"
+        },
+        {
+            "name": "미디어윌 그룹사 PC/MO 신규구축",
+            "participation": "100%",
+            "description": "사이트 노후로 인한 전체개편, 임원진 타겟팅 페이지 (html/css3/jQuery)",
+            "thumb" : "/src/image/thumb6.jpg",
+            "pageUrl" : "http://www.mediawill.com , http://m.mediawill.com"
+        },
+        {
+            "name": "알바천국 채용정보 전체개편",
+            "participation": "100%",
+            "description": "매출증대를 위해 서비스 개편, 크로스브라우징(IE7), BEM 방법론 적용 (html/css/jQuery)",
+            "thumb" : "/src/image/thumb7.jpg",
+            "pageUrl" : "http://www.alba.co.kr/job/Main.asp"
+        },
+        {
+            "name": "알바천국 인재정보 전체개편",
+            "participation": "100%",
+            "description": "매출증대를 위해 서비스 개편, 크로스브라우징(IE7), BEM 방법론 적용 (html/css/jQuery)",
+            "thumb" : "/src/image/thumb8.jpg",
+            "pageUrl" : "http://www.alba.co.kr/resume/Main.asp"
+        },
+        {
+            "name": "알바천국 개인서비스 전체개편",
+            "participation": "100%",
+            "description": "매출증대를 위해 서비스 개편, 크로스브라우징(IE7), BEM 방법론 적용 (html/css/jQuery)",
+            "thumb" : "/src/image/thumb9.jpg",
+            "pageUrl" : "https://www.alba.co.kr/person/main.asp"
+        },
+        {
+            "name": "강사닷컴 사이트 전체개편",
+            "participation": "100%",
+            "description": "사이트 노후로 인한 전체개편, 크로스브라우징(IE7), BEM 방법론 적용 (html/css/jQuery)",
+            "thumb" : "/src/image/thumb10.jpg",
+            "pageUrl" : "http://www.gangsa.com"
+        },
+        {
+            "name": "간호잡 사이트 전체개편",
+            "participation": "100%",
+            "description": "사이트 노후로 인한 전체개편, 크로스브라우징(IE7), BEM 방법론 적용 (html/css/jQuery)",
+            "thumb" : "/src/image/thumb11.jpg",
+            "pageUrl" : "http://www.ganhojob.com"
+        },
+        {
+            "name": "해양연구원 전체개편",
+            "participation": "60%",
+            "description": "사이트 개편, IE7 대응",
+            "thumb" : "/src/image/thumb12.jpg",
+            "pageUrl" : "http://www.kiost.ac.kr/kor.do"
+        },
+        {
+            "name": "과천과학관 전체개편",
+            "participation": "40%",
+            "description": "사이트 개편, IE7 대응",
+            "thumb" : "/src/image/thumb13.jpg",
+            "pageUrl" : "http://www.sciencecenter.go.kr/scipia"
+        },
+        {
+            "name": "롯데리아 홈서비스 전체개편",
+            "participation": "70%",
+            "description": "웹접근성 마크 획득, IE7대응",
+            "thumb" : "/src/image/thumb14.jpg",
+            "pageUrl":"https://mobilehome.lotteria.com"
+        },
+        {
+            "name": "한국과학기술연구원",
+            "participation": "40%",
+            "description": "웹접근성 마크 획득, IE7대응",
+            "thumb" : "/src/image/thumb15.jpg",
+            "pageUrl":"https://www.kist.re.kr/kist_web/main"
+        }
+    ]
+}
